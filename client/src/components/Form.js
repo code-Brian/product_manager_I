3c1b2418bf0ce0648478c237d5c1ae70ae2c3e45
Brian Denmark <testingarea479@gmail.com>
2022-11-05 17:03:35 -0700

    create form added

---
+++ client/src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Form = () => {
+    return (
+        <div>
+            <div className="bg-slate-500 p-3 flex flex-col items-center w-96 rounded">
+                <h2 className="font-bold">Create New Product</h2>
+                <form className="mx-auto p-2 flex flex-col content-center justify-center w-80">
+                    <div className="flex items-center justify-between">
+                        <label className="mr-2">Title:</label>
+                        <input className="mb-1 px-2 border rounded" type="text"></input>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <label className="">Price:</label>
+                        <input className="mb-1 px-2 border rounded" type="text"></input>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <label className="">Description:</label>
+                        <textarea className="mb-1 px-2 border rounded"></textarea>
+                    </div>
+                    <input className="self-center mt-3 w-32 bg-zinc-400 hover:bg-zinc-200 hover:text-black hover:cursor-pointer text-gray-50 px-2 border rounded" type="submit" value="Create Product"></input>
+                </form>
+            </div>
+        </div>
+    )
+}
